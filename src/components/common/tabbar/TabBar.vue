9fedf49fb121402e3aed7320d28c4a6a79f96cb8
yiwen0313 <2469119402@qq.com>
2021-11-08 20:16:54 +0800

    tabbar完成

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "TabBar"
+}
+</script>
+
+<style scoped>
+  #tab-bar {
+    display: flex;
+    height: 49px;
+    box-shadow: 0 -1px 2px rgba(0, 0, 0, .2);
+    background-color: #f6f6f6;
+
+    position: fixed;
+    right: 0;
+    left: 0;
+    bottom: 0;
+  }
+
+</style>
