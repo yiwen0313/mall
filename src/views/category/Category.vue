d3e4c5516402f1bfc03eac865fbd35d06a9cbdf5
yiwen0313 <2469119402@qq.com>
2021-11-15 21:33:16 +0800

    BetterScroll在vue中的使用

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -117,7 +117,20 @@ export default {
   },
   mounted() {
     this.scroll = new BScroll(document.querySelector('.wrapper'), {
+      probeType: 3,
+      pullUpLoad: true
+    })
 
+    this.scroll.on('scroll', (position) => {
+      // console.log(position);
+    })
+
+    this.scroll.on('pullingUp', () => {
+      console.log('上拉加载更多');
+
+      setTimeout(() => {
+        this.scroll.finishPullUp()
+      }, 2000)
     })
   }
 }
