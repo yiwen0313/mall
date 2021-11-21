1427d5f4057f1f00359e47f2a6642bc7f410060d
yiwen0313 <2469119402@qq.com>
2021-11-21 20:44:19 +0800

    详情页-轮播图展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
+    <detail-nav-bar/>
+    <detail-swiper :top-images="topImages"/>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
 
 import {getDetail} from "network/detail";
 
 export default {
   name: "Detail",
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper
   },
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: []
     }
   },
   created() {
@@ -25,7 +29,9 @@ export default {
 
     // 2.根据 iid请求详细数据
     getDetail(this.iid).then(res => {
-
+      // 2.1 获取顶部的图片轮播数据
+      // console.log(res);
+      this.topImages = res.result.itemInfo.topImages
     })
   }
 }
