4fe00501bd3804eaa63a03f4eb69ce38d418c0b7
yiwen0313 <2469119402@qq.com>
2021-11-22 20:18:50 +0800

    详情页-加入滚动的效果Scroll(引入better-scroll)

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="detail">
-    <detail-nav-bar/>
-    <detail-swiper :top-images="topImages"/>
-    <detail-base-info :goods="goods"/>
-    <detail-shop-info :shop="shop"/>
+    <detail-nav-bar class="detail-nav"/>
+    <scroll class="content">
+      <detail-swiper :top-images="topImages"/>
+      <detail-base-info :goods="goods"/>
+      <detail-shop-info :shop="shop"/>
+    </scroll>
   </div>
 </template>
 
@@ -13,6 +15,8 @@ import DetailSwiper from "./childComps/DetailSwiper";
 import DetailBaseInfo from "./childComps/DetailBaseInfo";
 import DetailShopInfo from "./childComps/DetailShopInfo";
 
+import Scroll from "components/common/scroll/Scroll";
+
 import {getDetail, Goods, Shop} from "network/detail";
 
 export default {
@@ -21,7 +25,8 @@ export default {
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
-    DetailShopInfo
+    DetailShopInfo,
+    Scroll
   },
   data() {
     return {
@@ -53,5 +58,17 @@ export default {
 </script>
 
 <style scoped>
+  #detail {
+    height: 100vh;
+  }
 
+  .detail-nav {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+  }
+
+  .content {
+    height: calc(100% - 44px);
+  }
 </style>
