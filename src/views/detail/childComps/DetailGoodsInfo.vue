0b6255ea4dffcd752c16a204b82a8fde3359a16f
yiwen0313 <2469119402@qq.com>
2021-11-23 21:20:20 +0800

    详情页-商品详情数据展示-小修改

--- src/views/detail/childComps/DetailGoodsInfo.vue
+++ src/views/detail/childComps/DetailGoodsInfo.vue
@@ -6,10 +6,11 @@
       <div class="end"></div>
     </div>
 
-    <div class="info-key">{{detailInfo.detailImage[0].key}}</div>
-
-    <div class="info-list" v-for="(item, index) in detailInfo.detailImage[0].list">
-      <img :src="item" alt="" @load="imgLoad">
+    <div v-for="item in detailInfo.detailImage">
+      <div class="info-key">{{item.key}}</div>
+      <div class="info-list">
+        <img v-for="(item, index) in item.list" :key="index" :src="item" alt="" @load="imgLoad">
+      </div>
     </div>
   </div>
 </template>
